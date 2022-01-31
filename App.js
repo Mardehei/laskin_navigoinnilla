ac5932ae547cb1e473cdb7f8935e347fba68a42c
Mari Paltiala <82866317+Mardehei@users.noreply.github.com>
2022-01-31 21:02:15 +0200

    historia

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button, RecyclerViewBackedScrollViewComponent } from 'react-native';
 
 
 export default function App() {
@@ -8,19 +8,29 @@ export default function App() {
   const [number1, setNumber1] = React.useState('');
   const [number2, setNumber2] = React.useState('');
   const [result, setResult] = React.useState('');
+  const [data, setData] = useState([]);
 
   const add = () => {
       setResult(parseInt(number1) + parseInt(number2));
+      const summa = (parseInt(number1) + parseInt(number2));
+      const text = number1 + " + " + number2 + " = " + summa;
+      setData([...data, { key: text }]);
+      setNumber1('');
+      setNumber2('');
   }
 
   const subtract = () => {
       setResult(parseInt(number1) - parseInt(number2));
+      const vahennys = (parseInt(number1) - parseInt(number2));
+      const text = number1 + " - " + number2 + " = " + vahennys;
+      setData([...data, { key: text }]);
+      setNumber1('');
+      setNumber2('');
   }
 
   return (
     <View style={styles.container}>
-      <Text>Laskin</Text>
-      <Text>Results: {result}</Text>
+      <Text>Result: {result}</Text>
       <SafeAreaView>
         <TextInput
           style={styles.input}
@@ -36,20 +46,27 @@ export default function App() {
           placeholder="Syötä toinen luku"
           keyboardType="numeric"
         />
-      <StatusBar style="auto"/>
+        <StatusBar style="auto"/>
       </SafeAreaView>
-      <View style={{ flexDirection: "row" }}>
-      <Button
-        title="+"
-        onPress={add}
-      />
-      <Button
-        title="-"
-        onPress={subtract}
-      />
+      <View style={styles.buttons}>
+        <Button
+          title="+"
+          onPress={add}
+        />
+        <View style={styles.space} />
+        <Button
+          title="-"
+          onPress={subtract}
+        />
       </View>
+      <Text style={styles.text}>History</Text>
+      <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <Text>{item.key}</Text>
+        )}
+        />
     </View>
-    
   );
 }
 
@@ -59,5 +76,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100,
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  flatlist: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    margin: 10,
+  },
+  space: {
+    width: 20,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+  }
 });
